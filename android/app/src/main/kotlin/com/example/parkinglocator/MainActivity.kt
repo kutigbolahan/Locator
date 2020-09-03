f973e01abba277302f42b1bb997fff70d80090fd
kuti gbolahan <kutigbolahan@ymail.com>
2020-09-03 13:31:37 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/parkinglocator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parkinglocator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:parkinglocator/screens/search.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Locator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Search(),
+    );
+  }
+}
